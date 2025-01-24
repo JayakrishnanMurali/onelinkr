41237e65761df5808b1e86d06f4d8c0ce9ede68c
jayakrishnanm <jayakrishnan1012@gmail.com>
2025-01-25 01:39:13 +0530

    initialize project

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+
+export default function DashboardPage() {
+  return (
+    <main className="">
+      <h1>this is dashboard</h1>
+    </main>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#026d50] to-[#152c1e] text-white">
+      <Button>
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
+    </main>
+  );
+}
